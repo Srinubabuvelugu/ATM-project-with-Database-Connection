d2e34db1e961d47898d0a34ff64e1bf73043711e
Srinubabuvelugu <121814804+Srinubabuvelugu@users.noreply.github.com>
2025-11-21 21:46:43 +0530

    Delete atm directory

--- atm/balance.py
+++
@@ -1,8 +0,0 @@
-import data as d
-from database.amount_fetch import AmountFetch
-def balance(account:int):
-    obj_af = AmountFetch(account=account)
-    curr_amount = obj_af.amountFetch()
-
-    return f"Current balance is {curr_amount}"
-    
